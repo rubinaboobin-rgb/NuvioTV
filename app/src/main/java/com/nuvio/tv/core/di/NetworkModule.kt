db184ccccc95282d871118a4289e3905a27b281b
liongalahad <giorgio.marinelli82@gmail.com>
2026-06-19 15:48:18 +1000

    Use public IMDb rating endpoint fallbacks

--- app/src/main/java/com/nuvio/tv/core/di/NetworkModule.kt
+++ app/src/main/java/com/nuvio/tv/core/di/NetworkModule.kt
@@ -86,6 +86,9 @@ private fun buildAcceptLanguageHeader(): String {
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
 
+    private const val DEFAULT_SERIES_GRAPH_API_BASE_URL = "https://seriesgraph.com/"
+    private const val DEFAULT_IMDB_TAPFRAME_API_BASE_URL = "https://imdb.tapframe.space/"
+
     @Provides
     @Singleton
     fun provideMoshi(): Moshi = Moshi.Builder()
@@ -517,7 +520,7 @@ object NetworkModule {
         val normalizedBaseUrl = if (rawBaseUrl.isNotBlank()) {
             if (rawBaseUrl.endsWith('/')) rawBaseUrl else "$rawBaseUrl/"
         } else {
-            "http://localhost/"
+            DEFAULT_SERIES_GRAPH_API_BASE_URL
         }
         return Retrofit.Builder()
             .baseUrl(normalizedBaseUrl)
@@ -539,7 +542,7 @@ object NetworkModule {
         val normalizedBaseUrl = if (rawBaseUrl.isNotBlank()) {
             if (rawBaseUrl.endsWith('/')) rawBaseUrl else "$rawBaseUrl/"
         } else {
-            "http://localhost/"
+            DEFAULT_IMDB_TAPFRAME_API_BASE_URL
         }
         return Retrofit.Builder()
             .baseUrl(normalizedBaseUrl)
